d2f637a4113f49b964823e828bc1e010a541ab85
Sy <simon.tsegay@gmail.com>
2019-02-11 19:41:31 -0500

    Updates

---
+++ precache-manifest.30b5bb5550d57aba184d619fb49e572f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f34a11bbb873481180d0",
+    "url": "/react-to-do-list/static/css/main.b9abf2ef.chunk.css"
+  },
+  {
+    "revision": "f34a11bbb873481180d0",
+    "url": "/react-to-do-list/static/js/main.f34a11bb.chunk.js"
+  },
+  {
+    "revision": "1015926caf23046f2ba3",
+    "url": "/react-to-do-list/static/js/1.1015926c.chunk.js"
+  },
+  {
+    "revision": "52435d08ff1beb8e480e",
+    "url": "/react-to-do-list/static/js/runtime~main.52435d08.js"
+  },
+  {
+    "revision": "ca17c875d468530857409992b1db9ed9",
+    "url": "/react-to-do-list/index.html"
+  }
+];
