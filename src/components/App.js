8a7518b5d7b78df9e3257f6f8222c75a387a69e0
Sy <simon.tsegay@gmail.com>
2019-02-12 19:08:02 -0500

    updated styling with paper-like appearance

--- src/components/App.js
+++ src/components/App.js
@@ -9,15 +9,19 @@ class App extends Component {
       label: "Personal",
       items: [{
         id: 0,
-        description: "Do laundry.",
+        description: "Do laundry and fold clothes.",
         isComplete: false
       }, {
         id: 1,
-        description: "Shovel snow off driveway, pour salt.",
+        description: "Shovel snow off driveway and pour salt.",
         isComplete: false
       }, {
         id: 2,
-        description: "Get oil change.",
+        description: "Take car to get oil change and smog check.",
+        isComplete: false
+      }, {
+        id: 2,
+        description: "Prep lunch meals for week.",
         isComplete: false
       }]
     }, {
@@ -25,15 +29,11 @@ class App extends Component {
       label: "Family",
       items: [{
         id: 0,
-        description: "Send flowers to wife.",
+        description: "Send flowers to wife for Valentine's Day.",
         isComplete: false
       }, {
         id: 1,
         description: "Make appointment with son's teacher.",
-        isComplete: false
-      }, {
-        id: 2,
-        description: "Prep lunch meals for week.",
         isComplete: false
       }]
     }, {
@@ -63,12 +63,34 @@ class App extends Component {
       // <List> renders out a list label and an array of <ListItem>
       // <ListItem> renders out an item description
       <div className="app">
-        <div className="vertical-red-lines"/>
-        <p className="app-title">To-Do List</p>
+        <div style={verticalRedLines} />
+        <p style={appTitleStyle}>To-Do List Manager</p>
         <Lists lists={lists} />
       </div>
     );
   }
+}
+
+// left set of red lines to imitate the appearance of paper
+const verticalRedLines = {
+  borderLeft: `0.5px solid #ffaa9f`,
+  borderRight: `0.5px solid #ffaa9f`,
+  width: `0.2em`,
+  height: `100vh`,
+  float: `left`,
+  marginLeft: `4em`
+}
+
+// add styling to app title so that it appears above paper's 1st line
+const appTitleStyle = {
+  color: `#666`,
+  fontSize: `3em`,
+  margin: `0`,
+  padding: `0`,
+  textIndent: `0.1em`,
+  paddingTop: `0.8em`,
+  lineHeight: `0.8em`,
+  borderBottom: `1px solid lightblue`
 }
 
 export default App;
