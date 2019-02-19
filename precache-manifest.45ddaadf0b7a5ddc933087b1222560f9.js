0047391196adfb846225982a73b9ef17c2556a2c
Sy <simon.tsegay@gmail.com>
2019-02-19 02:16:08 -0500

    Updates

--- precache-manifest.45ddaadf0b7a5ddc933087b1222560f9.js
+++ precache-manifest.45ddaadf0b7a5ddc933087b1222560f9.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "72704e76a610a52b090f",
+    "revision": "a3e6c566be85f2457eb9",
     "url": "/react-to-do-list/static/css/main.f471b4a9.chunk.css"
   },
   {
-    "revision": "72704e76a610a52b090f",
-    "url": "/react-to-do-list/static/js/main.72704e76.chunk.js"
+    "revision": "a3e6c566be85f2457eb9",
+    "url": "/react-to-do-list/static/js/main.a3e6c566.chunk.js"
   },
   {
     "revision": "79a43ede4a9ece8ffb44",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/react-to-do-list/static/js/runtime~main.52435d08.js"
   },
   {
-    "revision": "a1ab4a78baaccff16aa2481fd94e2313",
+    "revision": "9043f931bed91eb5cdb5b0f2438630c8",
     "url": "/react-to-do-list/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-to-do-list/precache-manifest.c1ea39c273396672fbc025ed8a429591.js"
+  "/react-to-do-list/precache-manifest.45ddaadf0b7a5ddc933087b1222560f9.js"
 );
 
 workbox.clientsClaim();
